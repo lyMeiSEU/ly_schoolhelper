852e92cb6b01364dbc47bee3e753cc2bd76396bb
lyMeiSEU <213173625@seu.edu.cn>
2019-05-10 16:12:36 +0800

    10/05/2019

--- 流量分析/N.py
+++ 流量分析/N.py
@@ -1,5 +1,10 @@
 import csv
 with open('./数据包/01C.csv') as f:
     f_csv=csv.reader(f)
+    dict={'TCPNo':'SIZE'}
+    var=""
     for row in f_csv:
-        print(row,"\n")
+        var1=row
+        var=var+var1
+    print(var)
+
---
+++ 流量分析/RDNS.py
@@ -0,0 +1,15 @@
+from dns import resolver
+
+ 
+
+ans = resolver.query("www.baidu.com", "A")
+
+print("qname:",ans.qname,"\n")
+
+print ("reclass:",ans.rdclass,"\n")
+
+print ("rdtype:",ans.rdtype,"\n")
+
+print ("rrset:",ans.rrset,"\n")
+
+print ("response:",ans.response,"\n")
